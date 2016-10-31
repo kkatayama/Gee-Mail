c2af99044f8ff0966de20e3055d1e718c81ec43d
kkatayama <katayama@udel.edu>
2016-10-31 00:13:19 -0400

    Added AES-256-CFB-MODE Encryption

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Gee-Mail
 A secure asynchronous (same-machine) message platform
+
+
+#ubuntu
+>###Install Libraries
+>sudo apt-get install libcrypto++-dev libcrypto++-doc libcrypto++-utils
+
+>###Compile
+>g++ -g3 -ggdb -O0 -DDEBUG -I/usr/include/cryptopp *.cpp sha256/*.cpp -o gee-mail -lcryptopp -lpthread
+
+#osx
+###Install Libraries
+brew install boost
+brew install cryptopp
+
+###Complie
+g++ -I/usr/local/include/cryptopp *.cpp sha256/*.cpp -o gee-mail
+g++ -g3 -ggdb -O0 -DDEBUG -I/usr/local/include/cryptopp *.cpp sha256/*.cpp -o gee-mail -lcryptopp -lpthread
+
--- gee-mail.cpp
+++ gee-mail.cpp
@@ -1,32 +1,102 @@
-#include "sha256/sha256.h"
+#include <boost/algorithm/string.hpp>
+#include <iomanip>
 #include <iostream>
 #include <stdio.h>
 #include <cstdlib>
 #include <string>
-//#include <boost/algorithm/string.hpp>
+#include "aes.h"
+#include "hex.h"
+#include "modes.h"
+#include "osrng.h"
+#include "filters.h"
+#include "cryptlib.h"
+#include "sha256/sha256.h"
 
 using namespace std;
+using namespace boost;
+using namespace CryptoPP;
 
 string secure_hash(string secret, string salt, int  stretch) {
   int i = 0;
-  string hash;// = "0";
-  string psalt = sha256(secret+salt);
+  string hash = "0";
+  string psalt = sha256(secret + salt);
 
   cout << "psalt  : " << psalt << endl;
   for (i = 0; i < stretch; i++) {
-    hash = sha256(hash+psalt);
+    hash = sha256(hash + psalt);
   }
   return hash;
 }
 
-int main () {
-
-  string test = sha256("hundreds");
+int main (int argc, char* argv[]) {
+  AutoSeededRandomPool prng;
+  
+  string test = sha256("0" + sha256("hundreds"));
   string ans = "A8677DF7AAC4E90428D7226C5D28971273A5DDDB2F0338C963ACC521A6232934";
   string test2 = secure_hash("hundreds","",1);
   
-  cout << "hundreds: " << test << endl;
+  cout << "test    : " << test << endl;
   cout << "ans     : " << ans << endl;
   cout << "test2   : " << test2 << endl;
+
+  if (iequals(test, test2)) {
+    cout << "hash matches" << endl;
+  }
+
+  string plaintext = "this is a test";
+  string cipher;
+  string encoded;
+  string recovered;
+  
+  /*********************************/
+  /* AES KEY AND IV INITIALIZATION */
+  /*********************************/  
+  byte key[ AES::DEFAULT_KEYLENGTH ];
+  prng.GenerateBlock(key, sizeof(key));
+  
+  byte iv[ AES::BLOCKSIZE ];
+  prng.GenerateBlock(iv, sizeof(iv));
+
+  // Pretty print key
+  encoded.clear();
+  StringSource(key, sizeof(key), true, new HexEncoder(new StringSink(encoded)));
+  cout << "key: " << encoded << endl;
+
+  // Pretty print iv
+  encoded.clear();
+  StringSource(iv, sizeof(iv), true, new HexEncoder(new StringSink(encoded)));
+  cout << "iv: " << encoded << endl;
+
+  /*********************************/
+  /*    AES CFB-MODE ENCRYPTION    */
+  /*********************************/  
+  try {
+    cout << "plain text: " << plaintext << endl;
+
+    CFB_Mode< AES >::Encryption e;
+    e.SetKeyWithIV(key, sizeof(key), iv);
+    StringSource(plaintext, true, new StreamTransformationFilter(e, new StringSink(cipher)));
+  } catch(const Exception& e) {
+    cerr << e.what() << endl;
+    exit(1);
+  }
+
+  encoded.clear();
+  StringSource(cipher, true, new HexEncoder(new StringSink(encoded)));
+  cout << "cipher text: " << encoded << endl;
+
+  /*********************************/
+  /*    AES CFB-MODE DECRYPTION    */
+  /*********************************/
+  try {
+    CFB_Mode< AES >::Decryption d;
+    d.SetKeyWithIV(key, sizeof(key), iv);
+    StringSource s(cipher, true, new StreamTransformationFilter(d, new StringSink(recovered)));
+      
+  } catch(const Exception& e) {
+    cerr << e.what() << endl;
+    exit(1);
+  }
+  
   return 0;
 }
