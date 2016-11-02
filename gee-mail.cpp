2ce65168a349ca409248a564124c0b703c4ae503
kkatayama <katayama@udel.edu>
2016-11-01 22:02:28 -0400

    Added SQLite3pp

--- gee-mail.cpp
+++ gee-mail.cpp
@@ -1,17 +1,22 @@
 #include <boost/algorithm/string.hpp>
+// #include <sqlite3.h> 
 #include <iostream>
 #include <stdio.h>
 #include <cstdlib>
 #include <string>
 #include "cryptogm/sha256.h"
 #include "cryptogm/cryptogm.h"
+#include "sqlite3pp/sqlite3pp.h"
 
 using namespace std;
 using namespace boost;
+using namespace sqlite3pp;
 
 int main (int argc, char* argv[]) {
   string *cipherdata = new string[3];
   string recovered;
+
+  // TEST SHA-256
   string test = sha256("0" + sha256("hundreds"));
   string ans = "A8677DF7AAC4E90428D7226C5D28971273A5DDDB2F0338C963ACC521A6232934";
   string test2 = secure_hash("hundreds","",1);
@@ -24,8 +29,9 @@ int main (int argc, char* argv[]) {
     cout << "hash matches" << endl;      
   }
 
+  // TEST AES-256 
   string plaintext = "this is a test message to see stuff";
-  string passphrase = secure_hash("This is a test passphrase that is really really super long to see what happens if I go out of bounds", "┌∩┐(◣_◢)┌∩┐", 1000);
+  string passphrase = secure_hash("This is a test passphrase", "┌∩┐(◣_◢)┌∩┐", 1000);
 
   cout << "pt: " << plaintext << endl;
   cipherdata = encrypt(plaintext, passphrase);
@@ -39,6 +45,26 @@ int main (int argc, char* argv[]) {
   recovered = decrypt(ct, kt, it);
 
   cout << "Decrypted Text: " << recovered << endl;
-  return 0;
-  
+
+
+  // SQLite3 INSERT TEST
+  database db("auth.db");
+
+  string username = "testUser";
+  string password = "123456789";
+  command cmd(db, "INSERT INTO users (username, password) VALUES (:username, :password)");
+  cmd.bind(":username", username, nocopy);
+  cmd.bind(":password", password, nocopy);
+  cmd.execute();
+
+  // SQLite3 SELECT TEST
+  query qry(db, "SELECT password FROM users WHERE username = :user");
+  qry.bind(":user", username, nocopy);
+  for (auto v : qry) {
+    string pw;
+    v.getter() >> pw;
+    cout << "\t" << pw << endl;
+    
+  }
+  return 0;  
 }
