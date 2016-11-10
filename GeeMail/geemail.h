040630291fddd8c7f5d78981c333f0d3ceffd0c7
kkatayama <katayama@udel.edu>
2016-11-09 19:32:48 -0500

    update GeeMail.h library

--- GeeMail/geemail.h
+++ GeeMail/geemail.h
@@ -6,7 +6,6 @@ std::vector <std::string> getSenders(std::string receiver);
 std::vector <std::string> getMessages(std::string receiver);
 std::string getTime();
 void writeMessage(std::string username, std::string receiver, std::string title, std::string message, std::string writetime, std::string readtime, std::string passphrase);
-bool checkSender(std::string send);
 bool check_password(std::string pass);
 int getChoice();
 bool userLogin(std::string username, std::string pass);
